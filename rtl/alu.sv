2be2c06d761df0bfc67635cde82dd36538a5f6c1
AustinWRye <RyeAustinW@gmail.com>
2020-06-07 01:12:57 -0400

    Add ALU control

--- rtl/alu.sv
+++ rtl/alu.sv
@@ -13,7 +13,7 @@ module alu
     ) (
         input  logic [DATA_WIDTH-1:0] a,        // operation data
         input  logic [DATA_WIDTH-1:0] b,        // operation data
-        input  logic [2:0]            op_sel,   // select operation
+        input  logic [1:0]            op_sel,   // select operation
         output logic [DATA_WIDTH-1:0] result,   // operation result
         output logic                  zero      // zero flag
     );
@@ -21,12 +21,11 @@ module alu
     always_comb
     begin
         case(op_sel)
-            3'b000: result = a + b; // add
-            3'b001: result = a - b; // sub
-            3'b010: result = a & b; // and
-            3'b011: result = a | b; // or
-            3'b100: if (a < b) result = 1;
-                    else       result = 0;
+            2'b00: result = a + b; // add
+            2'b01: result = a | b; // or
+            2'b10: result = a - b; // sub
+            2'b11: if (a < b) result = 1;
+                   else       result = 0;
             default: result = 0;
         endcase
     end
---
+++ rtl/alu_control.sv
@@ -0,0 +1,26 @@
+////////////////////////////////////////////////////////////////////////////////
+// Author: Austin Rye <ryeaustinw@gmail.com>
+//
+// Name: alu_control
+// Description:
+// ALU Control for controlling ALU operations
+////////////////////////////////////////////////////////////////////////////////
+
+module alu_control (
+        input  logic [1:0] alu_op,
+        input  logic [5:0] fn,
+        output logic [1:0] alu_control
+    );
+
+    always_comb
+        casex ({alu_op, fn})
+            8'b11xxxxxx: alu_control = 2'b01;
+            8'b00xxxxxx: alu_control = 2'b00;
+            8'b01xxxxxx: alu_control = 2'b10;
+            8'b10100000: alu_control = 2'b00;
+            8'b10100010: alu_control = 2'b10;
+            8'b10101010: alu_control = 2'b11;
+            default: alu_control = 2'b00;
+        endcase
+
+endmodule
