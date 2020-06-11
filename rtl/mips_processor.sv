2ab0132e582a86550f51255b7e70244547c39ecd
AustinWRye <RyeAustinW@gmail.com>
2020-06-11 00:28:48 -0400

    Add reg_file to mips_processor

--- rtl/mips_processor.sv
+++ rtl/mips_processor.sv
@@ -17,6 +17,12 @@ module mips_processor (
     // Instruction Memory
     logic [31:0] instr;
 
+    // Register File
+    logic        reg_write_en;
+    logic [4:0]  reg_write_addr;
+    logic [31:0] reg_write_data;
+    logic [4:0]  reg_read_addr_1, reg_read_addr_2;
+    logic [31:0] reg_read_data_1, reg_read_data_2;
 
     /////////////////////
     // Program Counter //
@@ -45,6 +51,29 @@ module mips_processor (
     ) u_instr_mem (
         .addr           (pc),
         .instr          (instr)
+    );
+
+    /////////////////////
+    //  Register File  //
+    /////////////////////
+
+    assign reg_read_addr_1 = instr[25:21];
+    assign reg_read_addr_2 = instr[20:16];
+    assign reg_write_addr  = instr[15:11];
+
+    reg_file #(
+        .DATA_WIDTH     (32),
+        .ADDR_WIDTH     (5)
+    ) u_reg_file (
+        .clk            (clk),
+        .rst            (rst),
+        .write_en       (reg_write_en),
+        .write_addr     (reg_write_addr),
+        .write_data     (reg_write_data),
+        .read_addr_1    (reg_read_addr_1),
+        .read_addr_2    (reg_read_addr_2),
+        .read_data_1    (reg_read_data_1),
+        .read_data_2    (reg_read_data_2)
     );
 
 endmodule
