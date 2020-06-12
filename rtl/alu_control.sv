f85ae996634f53588a5e01b1db5572bc4161f1d6
AustinWRye <RyeAustinW@gmail.com>
2020-06-11 20:02:23 -0400

    Add or, and, add, sub and slt operations to alu_control

--- rtl/alu_control.sv
+++ rtl/alu_control.sv
@@ -8,19 +8,18 @@
 
 module alu_control (
         input  logic [1:0] alu_op,     // alu opcode
-        input  logic [5:0] fn,         // function code
-        output logic [1:0] alu_control // alu control opcode
+        input  logic [5:0] func,       // function code
+        output logic [2:0] alu_control // alu control opcode
     );
 
     always_comb
-        casex ({alu_op, fn})
-            8'b11xxxxxx: alu_control = 2'b01;
-            8'b00xxxxxx: alu_control = 2'b00;
-            8'b01xxxxxx: alu_control = 2'b10;
-            8'b10100000: alu_control = 2'b00;
-            8'b10100010: alu_control = 2'b10;
-            8'b10101010: alu_control = 2'b11;
-            default: alu_control = 2'b00;
+        casex ({alu_op, func})
+            8'b1x100000: alu_control = 3'b010;
+            8'b1x100010: alu_control = 3'b110;
+            8'b1x100100: alu_control = 3'b000;
+            8'b1x100101: alu_control = 3'b001;
+            8'b1x101010: alu_control = 3'b111;
+            default: alu_control = 3'b000;
         endcase
 
 endmodule
