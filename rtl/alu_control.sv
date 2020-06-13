7663e37a21f69668a99a3312c86bcb46f7877e7a
AustinWRye <RyeAustinW@gmail.com>
2020-06-13 17:43:45 -0400

    Add lw and sw to alu_control

--- rtl/alu_control.sv
+++ rtl/alu_control.sv
@@ -14,11 +14,12 @@ module alu_control (
 
     always_comb
         casex ({alu_op, func})
-            8'b1x100000: alu_control = 3'b010;
-            8'b1x100010: alu_control = 3'b110;
-            8'b1x100100: alu_control = 3'b000;
-            8'b1x100101: alu_control = 3'b001;
-            8'b1x101010: alu_control = 3'b111;
+            8'b00xxxxxx: alu_control = 3'b010; // lw, sw
+            8'b1x100000: alu_control = 3'b010; // add
+            8'b1x100010: alu_control = 3'b110; // sub
+            8'b1x100100: alu_control = 3'b000; // and
+            8'b1x100101: alu_control = 3'b001; // or
+            8'b1x101010: alu_control = 3'b111; // slt
             default: alu_control = 3'b000;
         endcase
 
