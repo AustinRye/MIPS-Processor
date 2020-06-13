4f3e788433796d8bcf2ba816f0567b38099dfae0
AustinWRye <RyeAustinW@gmail.com>
2020-06-13 16:37:08 -0400

    Add lw instruction to control

--- rtl/control.sv
+++ rtl/control.sv
@@ -9,18 +9,34 @@
 module control (
         input  logic [5:0] opcode,    // instruction opcode
         output logic       reg_write, // register write
-        output logic [1:0] alu_op     // alu opcode
+        output logic       alu_src,   // alu source
+        output logic [1:0] alu_op,    // alu opcode
+        output logic       mem_read,  // memory read
+        output logic       mem_to_reg // memory to register
     );
 
     always_comb
         casex(opcode)
             6'b000000: begin // R-type
-                reg_write = 1'b1;
-                alu_op    = 2'b10;
+                reg_write  = 1'b1;
+                alu_src    = 1'b0;
+                alu_op     = 2'b10;
+                mem_read   = 1'b0;
+                mem_to_reg = 1'b0;
+            end
+            6'b100011: begin // lw
+                reg_write  = 1'b1;
+                alu_src    = 1'b1;
+                alu_op     = 2'b00;
+                mem_read   = 1'b1;
+                mem_to_reg = 1'b1;
             end
             default: begin
                 reg_write = 1'b0;
+                alu_src    = 1'b0;
                 alu_op    = 2'b00;
+                mem_read   = 1'b0;
+                mem_to_reg = 1'b0;
             end
         endcase
 
